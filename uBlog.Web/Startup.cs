50d0f5ef8c8b6c007bf9b59428d6c340ca955bf9
Ivan Muzyka <musicvano@gmail.com>
2018-05-18 03:15:47 +0300

    Update all packages

--- uBlog.Web/Startup.cs
+++ uBlog.Web/Startup.cs
@@ -74,7 +74,7 @@ namespace uBlog.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            //loggerFactory.AddConsole();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
